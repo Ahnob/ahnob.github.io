d2edb9a5de29cb7069d05a6d7b6baaa33ab6b409
Anob-ejah Takon <anobtakon@gmail.com>
2022-02-09 07:17:27 +0000

    project

---
+++ sololearn website/day6/js/index.js
@@ -0,0 +1,65 @@
+var domH1 = document.getElementById ("dom");
+var para = document.getElementById("para");
+// domH1.innerHTML = "DOM";
+dom.style.color= "purple";
+dom.style.fontSize ="25px"
+dom.style.backgroundColor ="#66bfbf";
+dom.style.display= "flex";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dom.adEventListener("click", function () {
+//     alert(dom.style.display);
+// });
+
+
+// dom.onclick = function () {
+//     alert("Hey there!");
+// }
+
+// function addition(parameter){body of the function}
+// creating a function
+
+// function addition(input) {
+//     let x = input;
+//     let y = 50;
+//     let z = x + y;
+//     return z
+// }
+
+// let answer = addition
+// let answer1 = addition(53)
+// let answer2 = addition(5)
+// let answer3 = addition(32)
+// let answer4 = addition(10)
+// let answer5 = addition(5)
+
+// console.log (answer); // 109 
+// console.log (answer1); //103
+// console.log (answer2); //55
+// console.log (answer3); //82
+// console.log (answer4); //60
+// console.log (answer5); //55
+
+
+
+// alert(res);
+
+// A return is a value that a function returns to the calling script or function when it completes its task. A return value can be any one of the four variable types: handle, integer, object, or string
