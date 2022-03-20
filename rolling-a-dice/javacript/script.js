7ce8ba5574523b43932f05a245de9e0629c0030b
Anob-Ejah Takon <anobtakon@gmail.com>
2022-03-20 21:49:36 +0100

    rollingdice

---
+++ rolling-a-dice/javacript/script.js
@@ -0,0 +1,13 @@
+// let randomNumber = Math.floor(Math.random() * 6) + 1;
+// console.log(randomNumber);
+document.querySelector(".btn").addEventListener("click", GameStart);
+
+function GameStart(){
+    const randomDice = GetRandomNumbers()
+    let imageTag = document.querySelector(".img1");
+    imageTag.src = "assets/dice" + randomDice + ".png";
+}
+
+function GetRandomNumbers(){
+    return Math.floor(Math.random()*6)+1;
+}
