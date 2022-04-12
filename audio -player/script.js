a9edc7b419334d34708c8657b6ef4f00828b342c
Anob-Ejah Takon <anobtakon@gmail.com>
2022-04-12 14:16:35 +0100

    mini-audio-audio

---
+++ audio -player/script.js
@@ -0,0 +1,21 @@
+let pauseIcon = document.querySelector("#pause-icon");
+let playIcon = document.querySelector("#play-icon");
+
+let audio = new Audio("music/song.mp3");
+
+playIcon.addEventListener("click", function () {
+  pauseIcon.style.display = "block";
+  playIcon.style.display = "none";
+  audio.play();
+});
+
+pauseIcon.addEventListener("click", function () {
+  playIcon.style.display = "block";
+  pauseIcon.style.display = "none";
+  audio.pause();
+});
+
+// $("#play-icon").on("click", function () {
+//   $("pause-icon").toggle();
+// });
+
