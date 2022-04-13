1e896659f7eac326ec016df2d62cb7344502d83a
Anob-Ejah Takon <anobtakon@gmail.com>
2022-04-13 11:43:45 +0100

    used jquery

--- audio -player/script.js
+++ audio -player/script.js
@@ -1,21 +1,32 @@
-let pauseIcon = document.querySelector("#pause-icon");
-let playIcon = document.querySelector("#play-icon");
+// let pauseIcon = document.querySelector("#pause-icon");
+// let playIcon = document.querySelector("#play-icon");
 
-let audio = new Audio("music/song.mp3");
+// let audio = new Audio("music/song.mp3");
 
-playIcon.addEventListener("click", function () {
-  pauseIcon.style.display = "block";
-  playIcon.style.display = "none";
-  audio.play();
-});
-
-pauseIcon.addEventListener("click", function () {
-  playIcon.style.display = "block";
-  pauseIcon.style.display = "none";
-  audio.pause();
-});
-
-// $("#play-icon").on("click", function () {
-//   $("pause-icon").toggle();
+// playIcon.addEventListener("click", function () {
+//   pauseIcon.style.display = "block";
+//   playIcon.style.display = "none";
+//   audio.play();
 // });
 
+// pauseIcon.addEventListener("click", function () {
+//   playIcon.style.display = "block";
+//   pauseIcon.style.display = "none";
+//   audio.pause();
+//   audio.currentTime = 0;
+// });
+
+let audio = new Audio("music/song.mp3");
+$("#play-icon").on("click", function () {
+  $("#pause-icon").show();
+  $("#play-icon").hide();
+  audio.play();
+  // $("#play-icon").animate({ opacity: "0.5" });
+  audio.currentTime = 0;
+});
+
+$("#pause-icon").on("click", function () {
+  $("#play-icon").toggle();
+  $("#pause-icon").hide();
+  audio.pause();
+});
