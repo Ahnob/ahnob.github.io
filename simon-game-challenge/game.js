3c09f3fc9849da7e0604d583c5aaeba301cf954f
Anob-Ejah Takon <anobtakon@gmail.com>
2022-04-16 01:57:11 +0100

    created random colours to match ids

--- simon-game-challenge/game.js
+++ simon-game-challenge/game.js
@@ -1,51 +1,3 @@
-// let buttons = document.querySelectorAll(".btn");
-// let green = new Audio("sounds/green.mp3");
-// $(".green").on("click", function () {
-//   $(".green").addClass("pressed");
-//   setTimeout(function () {
-//     $(".green").removeClass("pressed");
-//   }, 100);
-//   green.play();
-// });
-
-// let red = new Audio("sounds/red.mp3");
-// $(".red").on("click", function () {
-//   $(".red").addClass("pressed");
-//   setTimeout(function () {
-//     $(".red").removeClass("pressed");
-//   }, 100);
-//   red.play();
-// });
-
-// let yellow = new Audio("sounds/yellow.mp3");
-// $(".yellow").on("click", function () {
-//   $(".yellow").addClass("pressed");
-//   setTimeout(function () {
-//     $(".yellow").removeClass("pressed");
-//   }, 100);
-//   yellow.play();
-// });
-
-// let blue = new Audio("sounds/blue.mp3");
-// $(".blue").on("click", function () {
-//   $(".blue").addClass("pressed");
-//   setTimeout(function () {
-//     $(".blue").removeClass("pressed");
-//   }, 100);
-//   blue.play();
-// });
-
-// let btns = document.querySelectorAll(".btn");
-// let audio = new Audio("sounds/blue.mp3");
-// let numberOfButtons = btns.length;
-
-// for (let i = 0; i < numberOfButtons; i++) {
-// //   let btn = btns[i];
-//   btns[i].addEventListener("click", function (event) {
-//     audio.play();
-//   });
-// }
-
 let userClickedPattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
@@ -60,9 +12,7 @@ function nextSequence() {
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  console.log("audio");
-  audio.play();
+  
 }
 
 function changeTitle(title) {
@@ -78,41 +28,17 @@ $(document).on("keypress", function () {
   startGame();
 });
 
-// document.querySelector(".btn").addEventListener("click", nextSequence);
-// function nextSequence() {
-//   let green = new Audio("sounds/green.mp3");
-// $(".green").on("click", function () {
-//   $(".green").addClass("pressed");
-//   setTimeout(function () {
-//     $(".green").removeClass("pressed");
-//   }, 100);
-//   green.play();
-// });
+$(".btn").on("click", function (event) {
+  let userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  playSound()
+});
 
-// let red = new Audio("sounds/red.mp3");
-// $(".red").on("click", function () {
-//   $(".red").addClass("pressed");
-//   setTimeout(function () {
-//     $(".red").removeClass("pressed");
-//   }, 100);
-//   red.play();
-// });
+function playSound(name){
+console.log(name);
 
-// let yellow = new Audio("sounds/yellow.mp3");
-// $(".yellow").on("click", function () {
-//   $(".yellow").addClass("pressed");
-//   setTimeout(function () {
-//     $(".yellow").removeClass("pressed");
-//   }, 100);
-//   yellow.play();
-// });
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 
-// let blue = new Audio("sounds/blue.mp3");
-// $(".blue").on("click", function () {
-//   $(".blue").addClass("pressed");
-//   setTimeout(function () {
-//     $(".blue").removeClass("pressed");
-//   }, 100);
-//   blue.play();
-// });
-// }
+  
+}
