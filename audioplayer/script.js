1da17ac1803148d75f10567b948b365f6f8b0f6d
Anob-Ejah Takon <anobtakon@gmail.com>
2022-04-27 22:18:52 +0100

    deleted the excessess

--- audioplayer/script.js
+++ audioplayer/script.js
@@ -1,27 +1,3 @@
-// let pauseIcon = document.querySelector("#pause-icon");
-// let playIcon = document.querySelector("#play-icon");
-
-// let audio = new Audio("music/song.mp3");
-
-// playIcon.addEventListener("click", function () {
-//   pauseIcon.style.display = "block";
-//   playIcon.style.display = "none";
-//   audio.play();n
-// });
-
-// pauseIcon.addEventListener("click", function () {
-//   playIcon.style.display = "block";
-//   pauseIcon.style.display = "none";
-//   audio.pause();
-//   audio.currentTime = 0;
-// });
-
-// let audio = new Audio("music/tybello.mp3");
-// 
-
-
-
-
 const musicContainer = document.querySelector(".music-container");
 const playBtn = document.querySelector("#play-icon");
 const prevBtn = document.getElementById("backward");
@@ -34,7 +10,7 @@ const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
 const songs = [
-  "Sia-Alive", " tybello", "Passenger", "Bruno_Mars", "Celine-Dion","jessiej","Ann-Marie"];
+  "Sia-Alive", " tybello", "Passenger", "Bruno_Mars", "Celine-Dion","jessiej", "Ann-Marie"];
 
 // Keeping track ofsongs
 let songIndex = 0
@@ -95,99 +71,4 @@ $("#pause-icon").on("click", function () {
   audio.pause();
   
 });
-
-
-// prevBtn.addEventListener("click", prevSong)
-// nextBtn.addEventListener("click", nextSong)
-
-// previous and next buttons
-
-// let songIndex = 2;
-
-// loadSong(songs[songIndex]);
-
-// function loadSong(song){
-//   tittle.innerText = song;
-//   audio.src = `music/${song}.mp3`;
-//   cover.src = `covers/${song}.jpg`;
-// }
-
-// // In other to get the previous song wen the backwards key is clicked
-
-// function prevSong(){
-//   songIndex --
-//   if (songIndex < 0){
-//     songIndex = songs.length -1;
-//   }
-
-//   loadSong(songs[songIndex]);
-//   playSound();
-// }
-
-// function nextSong() {
-//   songIndex++;
-//   if (songIndex > songs.length -1) {
-//     songIndex = 0;
-//   }
-
-//   loadSong(songs[songIndex]);
-
-//   playSound();
-// }
-
-// // implementing the progress Bar by adding an eventListener to the audio tag
-// // and the event called timeout
-
-// audio.addEventListener("timeupdate", updateProgress);
-
-// // While your music is playing, you may want to click anywhere on the progress bar and have the song skip to that point.
-
-// progressContainer.addEventListener("click", setProgress);
-
-
-// // creating setProgress function
-
-// function setProgress(e) {
-//   const width = this.clientWidth;
-//   const clickX = e.offsetX;
-//   const duration = audio.duration;
-
-//   audio.currentTime = (clickX / width) * duration;
-// }
-
-// // The last functionality we want to implement is making the music player automatically play the next song when the current song ends
-
-// audio.addEventListener("ended", nextSong);
-
-
-// changing the pause and play
-
-
-// playBtn.addEventListener('click', () =>{
-//   const isPlaying = musicContainer.classList.contains("play");
-
-//   if (isPlaying){
-//     pauseSong();
-//   } else {
-//     playSong();
-//   }
-// });
-
-// function playSong(){
-//  musicContainer.classList.add("playing")
-//  playBtn.querySelector("i.fas").classList.remove("fa-play");
-//  playBtn.querySelector("i.fas").classList.add("fa-pause");
-
-//  audio.play();
-
-// }
-
-
-// function pauseSong() {
-//   musicContainer.classList.add("playing");
-//   playBtn.querySelector("i.fas").classList.add("fa-play");
-//   playBtn.querySelector("i.fas").classList.remove("fa-pause");
-
-//   audio.pause();
-// }
 
