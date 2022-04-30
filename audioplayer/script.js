54ff79f6e20e21cbc6a310d377580a07e849b3bd
Anob-Ejah Takon <anobtakon@gmail.com>
2022-04-30 14:52:39 +0100

    added click event to the progress bar

--- audioplayer/script.js
+++ audioplayer/script.js
@@ -8,105 +8,95 @@ const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
-const rotate = document.querySelector("#rotate")
-
-
-
+const rotate = document.querySelector("#rotate");
 
 const songs = [
-  "Sia-Alive", "tybello", "Passenger", "Bruno_Mars", "Celine-Dion","jessiej", "Ann-Marie"];
-
+  "Sia-Alive",
+  "tybello",
+  "Passenger",
+  "Bruno_Mars",
+  "Celine-Dion",
+  "jessiej",
+  "Ann-Marie",
+];
 
 // Keeping track ofsongs
-let songIndex = 0
+let songIndex = 0;
 
 // loading songs initially
 loadSong(songs[songIndex]);
 
 // update songs
-function loadSong(song){
-  title.innerText = song
-  audio.src= `music/${song}.mp3`;
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
   cover.src = `covers/${song}.jpg`;
-  
 }
 
 function playSong() {
   musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.remove(fa-play)
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
-  
+  playBtn.querySelector("i.fas").classList.remove(fa - play);
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
 }
 
-function playSong() {
+function pauseSong() {
   musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.add(fa-play)
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
-  
+  playBtn.querySelector("i.fas").classList.add(fa - play);
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
 }
-
 
 playBtn.addEventListener("click", () => {
-  const isPlaying = musicContainer.classList.contains("play")
+  const isPlaying = musicContainer.classList.contains("play");
 
-  if(isPlaying){
-    pauseSong()
-
-  }else{
-    playSong()
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
   }
-})
+});
 
-// Making it 
-// rotate.addEventListener("click", function () {
-//   document.querySelector("#sample").classList.add("rotating");
-// });
-
-// adding click events to the forward and backward keys
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
 
-
-
 // Adding Event Listeners
 
-
 // To get the previous song
-function prevSong(){
+function prevSong() {
   songIndex--;
 
-  if (songIndex < 0){
-    songIndex = songs.length-1;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
   }
 
   loadSong(songs[songIndex]);
-
-  audio.play();
 }
-// To get the next song 
-function nextSong(){
-  songIndex++
+// To get the next song
+function nextSong() {
+  songIndex++;
 
-  if (songIndex > songs.length-1){
-    songIndex = 0
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
   }
 
-  loadSong(songs[songIndex])
-
-  audio.play()
+  loadSong(songs[songIndex]);
 }
 
 function updateProgress(e) {
-  const {duration, currentTime}= e.srcElement
-  const progressPercent = (currentTime/duration) * 100
-  progress.style.width = `${progressPercent}%`
+  const { duration, currentTime} = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
-function setProgress(e){
-  const width = this.clientWidth
-  const clickX = e.offsetX
-  const duration = audio.duration
-  audio.currentTime = (clickX / width)* duration
+
+audio.addEventListener("timeupdate", updateProgress);
+
+progressContainer.addEventListener("click", setProgress);
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
 }
 
 $("#play-icon").on("click", function () {
@@ -119,34 +109,6 @@ $("#pause-icon").on("click", function () {
   $("#play-icon").show();
   $("#pause-icon").hide();
   audio.pause();
-  
-})
+});
 // To make the song go to the next one when a sng finishes
 audio.addEventListener("ended", nextSong);
-
-
-// audio.addEventListener('timeupdate', updateProgress)
-
-// progressContainer.addEventListener("click", setProgress)
-
-
-
-// function updateProgress (e){
-//   const {duration, currentTime} = e.srcElement
-//   const progressPercent = (currentTime / duration) =100
-//   progress.style.width= `${progressPercent}%`
-// }
-
-// function setProgress(e){
-//   const width = this.cleanWidth
-//   const clickX = e.offSetX
-//   const duration = audio.duration
-
-//   audio.currentTime = (clickX/width) * duration
-
-// }
-
-
-
-// // to Make the song go to the next when it ends
-//  audio.addEventListener(`ended`, nextSong );
