bcebf3f2aa633300c725a14fcc950e19eadb6b89
Anob-Ejah Takon <anobtakon@gmail.com>
2022-05-07 17:02:25 +0100

    refactored the simons game

--- rolling-a-dice/javacript/script.js
+++ rolling-a-dice/javacript/script.js
@@ -1,6 +1,6 @@
 // let randomNumber = Math.floor(Math.random() * 6) + 1;
 // console.log(randomNumber);
-document.querySelector(".btn").addEventListener("click", GameStart);
+// document.querySelector(".btn").addEventListener("click", GameStart);
 
 function GameStart(){
     const randomDice = GetRandomNumbers()
@@ -11,3 +11,4 @@ function GameStart(){
 function GetRandomNumbers(){
     return Math.floor(Math.random()*6)+1;
 }
+    console.log(4);
--- simon-game-challenge/game.js
+++ simon-game-challenge/game.js
@@ -1,47 +1,38 @@
-let userClickedPattern = [];
-let buttonColours = ["red", "blue", "green", "yellow"];
-let gamePattern = [];
+// // When the user presses any key on the KeyboardEvent, start the game and change the text to level 1
+// // We need a keyboard event listener
+// We need a function called start game
+//  - change the text to level 1
+//  - call the generateNextRandomColor function
+// When the user clicks on any of the colors
+//   - If the game has not started, call the gameOver function
+//   - get the color that was clicked on
+//   - check if it was the right color by comparing it with the random color that was generated.
+//     - increase the level
+//     - call generateNextRandomColor again
+//   - if it was the wrong color
+//     - call the gameOver function
+//       - Red background with animation and  a sound
+//       - Change the text to Game Over. Press any key to restart
 
-function nextSequence() {
+let randomColor = "";
+document.addEventListener("keypress", gameStart);
+
+function gameStart() {
+  document.querySelector("#level-title").innerHTML = "Level 1";
+  generateNextRandomColor();
+}
+
+function generateNextRandomColor() {
   let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  console.log(randomChosenColour);
-
-  $("#" + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100);
-  
+  let colors = ["red", "yellow", "blue", "green"];
+  randomColor = colors[randomNumber];
+  playSoundAndAnimate();
 }
 
-function changeTitle(title) {
-  $("h1").text(title);
-}
-
-function startGame() {
-  changeTitle("Level 1");
-  nextSequence();
-}
-
-$(document).on("keypress", function () {
-  startGame();
-});
-
-$(".btn").on("click", function (event) {
-  let userChosenColor = event.target.id;
-  userClickedPattern.push(userChosenColor);
-  playSound()
-});
-
-function playSound(name){
-
-  let audio = new Audio("sounds/" + name + ".mp3");
+function playSoundAndAnimate() {
+  let audio = new Audio("/sounds/" + randomColor + ".mp3");
   audio.play();
-
-  playSound(randomChosenColour);
+  $("#" + randomColor).animate({ opacity: "0.5" }, 300, function () {
+    $("#" + randomColor).animate({ opacity: "1" });
+  });
 }
-
-function animatePress(currentColour){
-}
-
