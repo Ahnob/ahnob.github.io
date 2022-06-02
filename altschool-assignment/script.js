d681eb1f4f76fd0b4f915f1e6a4f10bed240e8be
Anob-Ejah Takon <anobtakon@gmail.com>
2022-06-02 05:25:36 +0100

    successfully added the haburger and added click listener to it

--- altschool-assignment/script.js
+++ altschool-assignment/script.js
@@ -1,4 +1,16 @@
 function onClickHamburger() {
   document.querySelector("#hamburger").classList.toggle("change");
+
+  document.querySelector(".nav-menu").classList.toggle("active")
 }
  
+
+// function myFunction() {
+//   var x = document.querySelector(".nav-link");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
