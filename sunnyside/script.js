4a4692e97a7d9308dc76d33e4fa291c4ae24644e
Anob-Ejah Takon <anobtakon@gmail.com>
2022-06-05 23:35:34 +0100

    changed font color for a few paragraphs

--- sunnyside/script.js
+++ sunnyside/script.js
@@ -1,6 +1,6 @@
-const hamburger=document.getElementById('hamburger');
-const hamburger=document.getElementById('nav-ul');
+const hamburger = document.querySelector(".hamburger");
+const navUL = document.querySelector("#nav-ul");
 
-hamburger.addEventListener('click',() => {
-  navUL.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+  navUL.classList.toggle("active");
 });
