c73a27ea9cb0d6f7a76c2b25e7b2e57094b9f7da
Anob-Ejah Takon <anobtakon@gmail.com>
2022-06-23 13:10:12 +0100

    added the mouseover function to the arrow immages

---
+++ drop-down/design/script.js
@@ -0,0 +1,19 @@
+let subNav = document.querySelector(".arrow-up");
+let element = document.querySelector(".drop-down");
+
+document
+  .querySelectorAll(".drop-down")
+  .addEventListener("mouseover", function () 
+  {
+    alert("I a ");
+  });
+
+// element.addEventListener("click", function() {
+//     alert("I a  genuis");
+// }
+
+// function myFunction() {
+//   element.classList.toggle("");
+// }
+
+// alert("I am a genuis");
